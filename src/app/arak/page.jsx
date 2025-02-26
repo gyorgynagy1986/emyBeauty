24add59da5a8159873157f0ed17246d97790eebd
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2025-02-26 17:38:10 +0100

    services

--- src/app/arak/page.jsx
+++ src/app/arak/page.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import PriceTable from '../../components/layouts/prices/PriceTable'
+//mport React from 'react'
+//mport PriceTable from '../../components/layouts/prices/PriceTable'
+//
+//xport const metadata = {
+// title: 'Árak - Emy kozmetika',
+// openGraph: {
+//   title: 'Árak - Emy kozmetika',
+//   description: 'Árak',
+// },
+//
+//
+//onst page = () => {
+// return (
+//   <>
+//   <PriceTable />
+//   </>
+// )
+//
+//
+//xport default page
 
-export const metadata = {
-  title: 'Árak - Emy kozmetika',
-  openGraph: {
-    title: 'Árak - Emy kozmetika',
-    description: 'Árak',
-  },
-}
+import React from 'react'
 
 const page = () => {
   return (
-    <>
-    <PriceTable />
-    </>
+    <div>page</div>
   )
 }
 
