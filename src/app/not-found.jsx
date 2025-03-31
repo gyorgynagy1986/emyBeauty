3667c4e2eaec82fb8543afe8ec76eedaa5767217
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2025-03-31 11:37:39 +0200

    update

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,12 +4,12 @@ import styles from "./NotFound.module.css";
 export default function NotFound() {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Page Not Found</h2>
+      <h2 className={styles.title}>Az oldal nem található</h2>
       <p className={styles.message}>
-        Sorry, the page you're looking for doesn't exist.
+      Az oldal nem található
       </p>
       <Link href="/" className={styles.homeLink}>
-        Return Home
+        Főoldal
       </Link>
     </div>
   );
